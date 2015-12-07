ce3cfdbfdcaf5c372f3d4f64a122c44dc24312a4
Goktug Yilmaz <goktugyil@gmail.com>
2015-12-06 22:22:28 -0800

    Added center property to CGFloat

--- Sources/CGFloatExtensions.swift
+++ Sources/CGFloatExtensions.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
+extension CGFloat {
+    /// EZSwiftExtensions
+    public var center: CGFloat { return (self / 2) }
+}
+
 /// EZSwiftExtensions
 public func degreesToRadians (angle: CGFloat) -> CGFloat {
     return (CGFloat (M_PI) * angle) / 180.0
 }
+
+
+
