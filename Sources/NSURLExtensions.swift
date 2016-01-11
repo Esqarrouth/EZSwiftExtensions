5c364d58b3059148824bcd1bbaa6f83918f4d294
furuyan <sp3b47z9@gmail.com>
2016-01-11 18:42:02 +0900

    Add NSURLExtensions

---
+++ Sources/NSURLExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  NSURLExtensions.swift
+//  EZSwiftExtensions
+//
+//  Created by furuyan on 2016/01/11.
+//  Copyright (c) 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import Foundation
+
+extension NSURL {
+    
+    /// EZSE: Returns convert query to Dictionary
+    public var queryParameters: [String: String]? {
+        guard let components = NSURLComponents(URL: self, resolvingAgainstBaseURL: true), queryItems = components.queryItems else {
+            return nil
+        }
+        
+        var parameters = [String: String]()
+        for item in queryItems {
+            parameters[item.name] = item.value
+        }
+        
+        return parameters
+    }
+}
