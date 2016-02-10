d9713b3900d8c6d43eaea186ef24faf90c5342e0
Lucas Farah <lucas.farah@me.com>
2016-02-09 16:06:48 -0800

    Make EZSE great again

--- Sources/EZSwiftExtensions.swift
+++ Sources/EZSwiftExtensions.swift
@@ -10,173 +10,194 @@ import UIKit
 //TODO: others standart video, gif
 
 public struct ez {
-  
-  /// EZSwift Extensions
-  public static var appDisplayName: String {
-    return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleDisplayName") as? String
-      ?? NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleName") as! String
-  }
-  
-  /// EZSwift Extensions
-  public static var appVersion: String {
-    return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
-  }
-  
-  /// EZSwiftExtensions
-  public static var appBuild: String {
-    return NSBundle.mainBundle().objectForInfoDictionaryKey(kCFBundleVersionKey as String) as! String
-  }
-  
-  /// EZSwiftExtensions
-  public static var appVersionAndBuild: String {
-    let version = appVersion, build = appBuild
-    return version == build ? "v\(version)" : "v\(version)(\(build))"
-  }
-  
-  /// EZSwiftExtensions - Gives you the VC on top so you can easily push your popups
-  public static var topMostVC: UIViewController? {
-    var presentedVC = UIApplication.sharedApplication().keyWindow?.rootViewController
-    while let pVC = presentedVC?.presentedViewController {
-      presentedVC = pVC
+
+    /// EZSwift Extensions
+    public static var appDisplayName: String {
+        return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleDisplayName") as? String
+            ?? NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleName") as! String
+    }
+
+    /// EZSwift Extensions
+    public static var appVersion: String {
+        return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
+    }
+
+    /// EZSwiftExtensions
+    public static var appBuild: String {
+        return NSBundle.mainBundle().objectForInfoDictionaryKey(kCFBundleVersionKey as String) as! String
+    }
+
+    /// EZSwiftExtensions
+    public static var appVersionAndBuild: String {
+        let version = appVersion, build = appBuild
+        return version == build ? "v\(version)" : "v\(version)(\(build))"
+    }
+
+    /// EZSwiftExtensions - Gives you the VC on top so you can easily push your popups
+    public static var topMostVC: UIViewController? {
+        var presentedVC = UIApplication.sharedApplication().keyWindow?.rootViewController
+        while let pVC = presentedVC?.presentedViewController {
+            presentedVC = pVC
+        }
+
+        if presentedVC == nil {
+            print("EZSwiftExtensions Error: You don't have any views set. You may be calling them in viewDidLoad. Try viewDidAppear instead.")
+        }
+        return presentedVC
+    }
+
+    /// EZSwiftExtensions
+    public static var screenOrientation: UIInterfaceOrientation {
+        return UIApplication.sharedApplication().statusBarOrientation
+    }
+
+    /// EZSwiftExtensions
+    public static var screenWidth: CGFloat {
+        if UIInterfaceOrientationIsPortrait(screenOrientation) {
+            return UIScreen.mainScreen().bounds.size.width
+        } else {
+            return UIScreen.mainScreen().bounds.size.height
+        }
+    }
+
+    /// EZSwiftExtensions
+    public static var screenHeight: CGFloat {
+        if UIInterfaceOrientationIsPortrait(screenOrientation) {
+            return UIScreen.mainScreen().bounds.size.height
+        } else {
+            return UIScreen.mainScreen().bounds.size.width
+        }
+    }
+
+    /// EZSwiftExtensions
+    public static var screenStatusBarHeight: CGFloat {
+        return UIApplication.sharedApplication().statusBarFrame.height
+    }
+
+    /// EZSwiftExtensions
+    public static var screenHeightWithoutStatusBar: CGFloat {
+        if UIInterfaceOrientationIsPortrait(screenOrientation) {
+            return UIScreen.mainScreen().bounds.size.height - screenStatusBarHeight
+        } else {
+            return UIScreen.mainScreen().bounds.size.width - screenStatusBarHeight
+        }
+    }
+
+    /// EZSwiftExtensions - Calls action when a screen shot is taken
+    public static func detectScreenShot(action: () -> ()) {
+        let mainQueue = NSOperationQueue.mainQueue()
+        NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationUserDidTakeScreenshotNotification, object: nil, queue: mainQueue) { notification in
+            // executes after screenshot
+            action()
+        }
+    }
+
+    // MARK: - Dispatch
+
+
+    /// EZSwiftExtensions
+    public static func runThisAfterDelay(seconds seconds: Double, after: () -> ()) {
+        runThisAfterDelay(seconds: seconds, queue: dispatch_get_main_queue(), after: after)
+    }
+
+    //TODO: Make this easier
+    /// EZSwiftExtensions - dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)
+    public static func runThisAfterDelay(seconds seconds: Double, queue: dispatch_queue_t, after: ()->()) {
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
+        dispatch_after(time, queue, after)
+    }
+
+    /// EZSwiftExtensions - Submits a block for asynchronous execution on the main queue
+    public static func runThisInMainThread(block: dispatch_block_t) {
+        dispatch_async(dispatch_get_main_queue(), block)
+    }
+
+    /// EZSwiftExtensions - Runs in Default priority queue
+    public static func runThisInBackground(block: () -> ()) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
     }
     
-    if presentedVC == nil {
-      print("EZSwiftExtensions Error: You don't have any views set. You may be calling them in viewDidLoad. Try viewDidAppear instead.")
+    /// EZSwiftExtensions - Runs every second, to cancel use: timer.invalidate()
+    public static func runThisEvery(seconds seconds: NSTimeInterval, handler: NSTimer! -> Void) -> NSTimer {
+        let fireDate =  CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, seconds, 0, 0, handler)
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+        return timer
     }
-    return presentedVC
-  }
-  
-  /// EZSwiftExtensions
-  public static var screenOrientation: UIInterfaceOrientation {
-    return UIApplication.sharedApplication().statusBarOrientation
-  }
-  
-  /// EZSwiftExtensions
-  public static var screenWidth: CGFloat {
-    if UIInterfaceOrientationIsPortrait(screenOrientation) {
-      return UIScreen.mainScreen().bounds.size.width
-    } else {
-      return UIScreen.mainScreen().bounds.size.height
+
+    // MARK: - DownloadTask
+
+    /// EZSwiftExtensions
+    public static func requestImage(url: String, success: (UIImage?) -> Void) {
+        requestURL(url, success: { (data) -> Void in
+            if let d = data {
+                success(UIImage(data: d))
+            }
+        })
     }
-  }
-  
-  /// EZSwiftExtensions
-  public static var screenHeight: CGFloat {
-    if UIInterfaceOrientationIsPortrait(screenOrientation) {
-      return UIScreen.mainScreen().bounds.size.height
-    } else {
-      return UIScreen.mainScreen().bounds.size.width
+
+    /// EZSwiftExtensions
+    public static func requestJSON(url: String, success: (AnyObject? -> Void), error: ((NSError) -> Void)?) {
+        requestURL(url,
+            success: { (data)->Void in
+                let json: AnyObject? = self.dataToJsonDict(data)
+                success(json)
+            },
+            error: { (err)->Void in
+                if let e = error {
+                    e(err)
+                }
+        })
     }
-  }
-  
-  /// EZSwiftExtensions
-  public static var screenStatusBarHeight: CGFloat {
-    return UIApplication.sharedApplication().statusBarFrame.height
-  }
-  
-  /// EZSwiftExtensions
-  public static var screenHeightWithoutStatusBar: CGFloat {
-    if UIInterfaceOrientationIsPortrait(screenOrientation) {
-      return UIScreen.mainScreen().bounds.size.height - screenStatusBarHeight
-    } else {
-      return UIScreen.mainScreen().bounds.size.width - screenStatusBarHeight
-    }
-  }
-  
-  /// EZSwiftExtensions - Calls action when a screen shot is taken
-  public static func detectScreenShot(action: () -> ()) {
-    let mainQueue = NSOperationQueue.mainQueue()
-    NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationUserDidTakeScreenshotNotification, object: nil, queue: mainQueue) { notification in
-      // executes after screenshot
-      action()
-    }
-  }
-  
-  // MARK: - Dispatch
-  
-  /// EZSwiftExtensions - Submits a block for asynchronous execution on the main queue
-  public static func runThisInMainThread(block: dispatch_block_t) {
-    dispatch_async(dispatch_get_main_queue(), block)
-  }
-  
-  /// EZSwiftExtensions - Runs in Default priority queue
-  public static func runThisInBackground(block: () -> ()) {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
-  }
-  
-  // MARK: - DownloadTask
-  
-  /// EZSwiftExtensions
-  public static func requestImage(url: String, success: (UIImage?) -> Void) {
-    requestURL(url, success: { (data) -> Void in
-      if let d = data {
-        success(UIImage(data: d))
-      }
-    })
-  }
-  
-  /// EZSwiftExtensions
-  public static func requestJSON(url: String, success: (AnyObject? -> Void), error: ((NSError) -> Void)?) {
-    requestURL(url,
-      success: { (data)->Void in
-        let json: AnyObject? = self.dataToJsonDict(data)
-        success(json)
-      },
-      error: { (err)->Void in
-        if let e = error {
-          e(err)
-        }
-    })
-  }
-  
-  /// EZSwiftExtensions
-  private static func dataToJsonDict(data: NSData?) -> AnyObject? {
-    if let d = data {
-      var error: NSError?
-      let json: AnyObject?
-      do {
-        json = try NSJSONSerialization.JSONObjectWithData(
-          d,
-          options: NSJSONReadingOptions.AllowFragments)
-      } catch let error1 as NSError {
-        error = error1
-        json = nil
-      }
-      
-      if let _ = error {
-        return nil
-      } else {
-        return json
-      }
-    } else {
-      return nil
-    }
-  }
-  
-  /// EZSwiftExtensions
-  private static func requestURL(url: String, success: (NSData?) -> Void, error: ((NSError) -> Void)? = nil) {
-    guard #available(iOS 9, *) else {
-      NSURLConnection.sendAsynchronousRequest(
-        NSURLRequest(URL: NSURL (string: url)!),
-        queue: NSOperationQueue.mainQueue(),
-        completionHandler: { response, data, err in
-          if let e = err {
-            error?(e)
-          } else {
-            success(data)
-          }
-      })
-      return
-    }
-    NSURLSession.sharedSession().dataTaskWithRequest(
-      NSURLRequest(URL: NSURL (string: url)!),
-      completionHandler: { data, response, err in
-        if let e = err {
-          error?(e)
+
+    /// EZSwiftExtensions
+    private static func dataToJsonDict(data: NSData?) -> AnyObject? {
+        if let d = data {
+            var error: NSError?
+            let json: AnyObject?
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(
+                    d,
+                    options: NSJSONReadingOptions.AllowFragments)
+            } catch let error1 as NSError {
+                error = error1
+                json = nil
+            }
+
+            if let _ = error {
+                return nil
+            } else {
+                return json
+            }
         } else {
-          success(data)
+            return nil
         }
-    }).resume()
-  }
-  
+    }
+
+    /// EZSwiftExtensions
+    private static func requestURL(url: String, success: (NSData?) -> Void, error: ((NSError) -> Void)? = nil) {
+        guard #available(iOS 9, *) else {
+            NSURLConnection.sendAsynchronousRequest(
+                NSURLRequest(URL: NSURL (string: url)!),
+                queue: NSOperationQueue.mainQueue(),
+                completionHandler: { response, data, err in
+                    if let e = err {
+                        error?(e)
+                    } else {
+                        success(data)
+                    }
+            })
+            return
+        }
+        NSURLSession.sharedSession().dataTaskWithRequest(
+            NSURLRequest(URL: NSURL (string: url)!),
+            completionHandler: { data, response, err in
+                if let e = err {
+                    error?(e)
+                } else {
+                    success(data)
+                }
+        }).resume()
+    }
+
 }
--- Sources/StringExtensions.swift
+++ Sources/StringExtensions.swift
@@ -8,145 +8,121 @@
 import UIKit
 
 extension String {
-  
-  /// EZSwiftExtensions
-  public subscript(integerIndex: Int) -> Character {
-    let index = startIndex.advancedBy(integerIndex)
-    return self[index]
-  }
-  
-  /// EZSwiftExtensions
-  public subscript(integerRange: Range<Int>) -> String {
-    let start = startIndex.advancedBy(integerRange.startIndex)
-    let end = startIndex.advancedBy(integerRange.endIndex)
-    let range = start..<end
-    return self[range]
-  }
-  
-  /// EZSwiftExtensions - Character count
-  public var length: Int {
-    return self.characters.count
-  }
-  
-  /// EZSwiftExtensions - counts number os instances of the input inside String
-  func count(substring: String) -> Int
-  {
-    return componentsSeparatedByString(substring).count-1
-  }
-  
-  /// EZSwiftExtensions
-  public var capitalizeFirst: String {
-    var result = self
-    result.replaceRange(startIndex...startIndex, with: String(self[startIndex]).capitalizedString)
-    return result
-  }
-  
-  /// EZSwiftExtensions - Counts whitespace & new lines
-  public func isOnlyEmptySpacesAndNewLineCharacters() -> Bool {
-    let characterSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
-    let newText = self.stringByTrimmingCharactersInSet(characterSet)
-    return newText.isEmpty
-  }
-  
-  /// EZSwiftExtensions - Trims white space and new line characters
-  public mutating func trim() {
-    self = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-  }
-  
-  /// EZSwiftExtensions - Returns if String is an Email
-  public var isEmail: Bool {
-    let dataDetector = try? NSDataDetector(types: NSTextCheckingType.Link.rawValue)
-    let firstMatch = dataDetector?.firstMatchInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, length))
-    return (firstMatch?.range.location != NSNotFound && firstMatch?.URL?.scheme == "mailto")
-  }
-  
-  /// EZSwiftExtensions - Returns if String is a number
-  func isNumeric() -> Bool {
-    if let _ = NSNumberFormatter().numberFromString(self) {
-      return true
-    }
-    return false
-  }
-  
-  /// EZSwiftExtensions
-  public var extractURLs: [NSURL] {
-    var urls : [NSURL] = []
-    let detector: NSDataDetector?
-    do {
-      detector = try NSDataDetector(types: NSTextCheckingType.Link.rawValue)
-    } catch _ as NSError {
-      detector = nil
+    
+    /// EZSwiftExtensions
+    public subscript(integerIndex: Int) -> Character {
+        let index = startIndex.advancedBy(integerIndex)
+        return self[index]
     }
     
-    let text = self
+    /// EZSwiftExtensions
+    public subscript(integerRange: Range<Int>) -> String {
+        let start = startIndex.advancedBy(integerRange.startIndex)
+        let end = startIndex.advancedBy(integerRange.endIndex)
+        let range = start..<end
+        return self[range]
+    }
     
-    detector!.enumerateMatchesInString(text, options: [], range: NSMakeRange(0, text.characters.count), usingBlock: {
-      (result: NSTextCheckingResult?, flags: NSMatchingFlags, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
-      urls.append(result!.URL!)
-    })
+    /// EZSwiftExtensions - Character count
+    public var length: Int {
+        return self.characters.count
+    }
+
+    /// EZSwiftExtensions
+    public var capitalizeFirst: String {
+        var result = self
+        result.replaceRange(startIndex...startIndex, with: String(self[startIndex]).capitalizedString)
+        return result
+    }
     
-    return urls
-  }
-  
-  /// EZSwiftExtensions
-  public func contains(find: String) -> Bool {
-    return self.rangeOfString(find) != nil
-  }
-  
-  /// EZSwiftExtensions
-  public func contains(find: String, compareOption: NSStringCompareOptions) -> Bool {
-    return self.rangeOfString(find, options: compareOption) != nil
-  }
-  
-  /// EZSwiftExtensions: Converts String to Int
-  public func toInt() -> Int? {
-    if let num = NSNumberFormatter().numberFromString(self) {
-      return num.integerValue
-    } else {
-      return nil
+    /// EZSwiftExtensions - Counts whitespace & new lines
+    public func isOnlyEmptySpacesAndNewLineCharacters() -> Bool {
+        let characterSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        let newText = self.stringByTrimmingCharactersInSet(characterSet)
+        return newText.isEmpty
     }
-  }
-  
-  /// EZSwiftExtensions: Converts String to Double
-  public func toDouble() -> Double? {
-    if let num = NSNumberFormatter().numberFromString(self) {
-      return num.doubleValue
-    } else {
-      return nil
-    }
-  }
-  
-  /// EZSwiftExtensions: Converts String to Float
-  public func toFloat() -> Float? {
-    if let num = NSNumberFormatter().numberFromString(self) {
-      return num.floatValue
-    } else {
-      return nil
-    }
-  }
-  
-  /// EZSwiftExtensions: Converts String to Bool
-  func toBool() -> Bool? {
-    let trimmed = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).lowercaseString
     
-    if trimmed == "true" || trimmed == "false" {
-      return (trimmed as NSString).boolValue
+    /// EZSwiftExtensions - Trims white space and new line characters
+    public mutating func trim() {
+         self = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
     }
-    return nil
-  }
-  
-  ///EZSE: Returns the first index of the occurency of the character in String
-  public func getIndexOf(char: Character) -> Int {
-    for (index, c) in characters.enumerate() {
-      if c == char {
-        return index
-      }
+    
+    /// EZSwiftExtensions
+    public var isEmail: Bool {
+        let dataDetector = try? NSDataDetector(types: NSTextCheckingType.Link.rawValue)
+        let firstMatch = dataDetector?.firstMatchInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, length))
+        return (firstMatch?.range.location != NSNotFound && firstMatch?.URL?.scheme == "mailto")
     }
-    return 0
-  }
-  
-  /// EZSwiftExtensions - Convert String to NSString
-  public var toNSString: NSString { get { return self as NSString } }
-  
+    
+    /// EZSwiftExtensions
+    public var extractURLs: [NSURL] {
+        var urls : [NSURL] = []
+        let detector: NSDataDetector?
+        do {
+            detector = try NSDataDetector(types: NSTextCheckingType.Link.rawValue)
+        } catch _ as NSError {
+            detector = nil
+        }
+
+        let text = self
+        
+        detector!.enumerateMatchesInString(text, options: [], range: NSMakeRange(0, text.characters.count), usingBlock: {
+            (result: NSTextCheckingResult?, flags: NSMatchingFlags, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+            urls.append(result!.URL!)
+        })
+        
+        return urls
+    }
+    
+    /// EZSwiftExtensions
+    public func contains(find: String) -> Bool {
+        return self.rangeOfString(find) != nil
+    }
+    
+    /// EZSwiftExtensions
+    public func contains(find: String, compareOption: NSStringCompareOptions) -> Bool {
+        return self.rangeOfString(find, options: compareOption) != nil
+    }
+    
+    /// EZSwiftExtensions
+    public func toInt() -> Int? {
+        if let num = NSNumberFormatter().numberFromString(self) {
+            return num.integerValue
+        } else {
+            return nil
+        }
+    }
+    
+    /// EZSwiftExtensions
+    public func toDouble() -> Double? {
+        if let num = NSNumberFormatter().numberFromString(self) {
+            return num.doubleValue
+        } else {
+            return nil
+        }
+    }
+    
+    /// EZSwiftExtensions
+    public func toFloat() -> Float? {
+        if let num = NSNumberFormatter().numberFromString(self) {
+            return num.floatValue
+        } else {
+            return nil
+        }
+    }
+    
+    ///EZSE: Returns the first index of the occurency of the character in String
+    public func getIndexOf(char: Character) -> Int {
+        for (index, c) in characters.enumerate() {
+            if c == char {
+                return index
+            }
+        }
+        return 0
+    }
+    
+    /// EZSwiftExtensions
+    public var toNSString: NSString { get { return self as NSString } }
+    
 }
 
