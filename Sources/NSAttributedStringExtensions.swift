5abac0cb5eb9302fd59827dc080df77f119d902e
Lucas Farah <lucas.farah@me.com>
2016-02-18 02:01:54 -0800

    Added NSAttributedStringExtensions.swift

---
+++ Sources/NSAttributedStringExtensions.swift
@@ -0,0 +1,43 @@
+//
+//  NSAttributedStringExtensions.swift
+//  EZSwiftExtensions
+//
+//  Created by Lucas Farah on 18/02/16.
+//  Copyright (c) 2016 Lucas Farah. All rights reserved.
+//
+import UIKit
+
+extension NSAttributedString {
+
+    /// EZSE: Adds bold attribute to NSAttributedString
+    func bold() -> NSAttributedString {
+        let copy = self.mutableCopy()
+        let range = (self.string as NSString).rangeOfString(self.string)
+        copy.addAttributes([NSFontAttributeName : UIFont.boldSystemFontOfSize(UIFont.systemFontSize())], range: range)
+        return copy as! NSAttributedString
+    }
+  
+    /// EZSE: Adds underline attribute to NSAttributedString
+    func underline() -> NSAttributedString {
+        let copy = self.mutableCopy()
+        let range = (self.string as NSString).rangeOfString(self.string)
+        copy.addAttributes([NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleSingle.rawValue], range: range)
+        return copy as! NSAttributedString
+    }
+  
+    /// EZSE: Adds italic attribute to NSAttributedString
+    func italic() -> NSAttributedString {
+        let copy = self.mutableCopy()
+        let range = (self.string as NSString).rangeOfString(self.string)
+        copy.addAttributes([NSFontAttributeName : UIFont.italicSystemFontOfSize(UIFont.systemFontSize())], range: range)
+        return copy as! NSAttributedString
+    }
+  
+    /// EZSE: Adds color attribute to NSAttributedString
+    func color(color:UIColor) -> NSAttributedString {
+        let copy = self.mutableCopy()
+        let range = (self.string as NSString).rangeOfString(self.string)
+        copy.addAttributes([NSForegroundColorAttributeName : color], range: range)
+        return copy as! NSAttributedString
+    }
+}
