b231281fe2e7eb2b1477ec1cd4ddeb230c24f354
X <troy@xinstitute.space>
2016-02-25 13:31:00 -0800

    Fix Doc for CGFloat Central

--- Sources/CGFloatExtensions.swift
+++ Sources/CGFloatExtensions.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 extension CGFloat {
-    /// EZSwiftExtensions
+    /// EZSE: Return the central value of CGFloat.
     public var center: CGFloat { return (self / 2) }
 }
 
-/// EZSE: Converts angle degrees to radians
+/// EZSE: Converts angle degrees to radians.
 public func degreesToRadians (angle: CGFloat) -> CGFloat {
     return (CGFloat (M_PI) * angle) / 180.0
 }
