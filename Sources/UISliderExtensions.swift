9f25388acea3bc4fa6bcf916a6ebe2a3159f7e69
Goktug Yilmaz <goktugyil@gmail.com>
2016-03-01 17:27:58 -0800

    Created UISlider file

---
+++ Sources/UISliderExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  UISliderExtensions.swift
+//  EZSwiftExtensions
+//
+//  Created by Goktug Yilmaz on 3/1/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+extension UISlider {
+
+}
