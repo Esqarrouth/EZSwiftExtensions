5ecf4f493e766acaad515b7ac9f192afea35f291
Goktug Yilmaz <goktugyil@gmail.com>
2016-03-01 17:52:45 -0800

    Added Character.toString()

---
+++ Sources/CharacterExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterExtensions.swift
+//  EZSwiftExtensions
+//
+//  Created by Goktug Yilmaz on 3/1/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import Foundation
+
+extension Character {
+    /// EZSE: Converts Character to String //TODO: Add to readme
+    public var toString: String { return String(self) }
+}
+
+
