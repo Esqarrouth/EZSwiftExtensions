d95455f5e1a5b4b5aeaa3bbed86eaee4a9056eeb
Lucas Farah <lucas.farah@me.com>
2016-03-01 19:16:20 -0800

    Updated initialization with hexString
    Deleted option with hex value: Int

--- Sources/UIColorExtensions.swift
+++ Sources/UIColorExtensions.swift
@@ -15,20 +15,14 @@ extension UIColor {
     }
 
     /// EZSwiftExtensions
-    public convenience init(hex: Int, alpha: CGFloat = 1.0) {
-        let red = CGFloat(CGFloat((hex & 0xFF0000) >> 16)/255.0)
-        let green = CGFloat(CGFloat((hex & 0x00FF00) >> 8)/255.0)
-        let blue = CGFloat(CGFloat((hex & 0x0000FF) >> 0)/255.0)
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-    
-    /// EZSwiftExtensions
     public convenience init?(hexString: String, alpha: CGFloat = 1.0) {
         var formatted = hexString.stringByReplacingOccurrencesOfString("0x", withString: "")
         formatted = formatted.stringByReplacingOccurrencesOfString("#", withString: "")
         if let hex = Int(formatted, radix: 16) {
-            self.init(hex: hex, alpha: alpha)
-        } else {
+          let red = CGFloat(CGFloat((hex & 0xFF0000) >> 16)/255.0)
+          let green = CGFloat(CGFloat((hex & 0x00FF00) >> 8)/255.0)
+          let blue = CGFloat(CGFloat((hex & 0x0000FF) >> 0)/255.0)
+          self.init(red: red, green: green, blue: blue, alpha: alpha)        } else {
             return nil
         }
     }
