569729c04ee3a3da48e60142f1ce034e5f95fbff
Goktug Yilmaz <goktugyil@gmail.com>
2016-03-01 18:25:30 -0800

    Added UIWindow.init()

---
+++ Sources/UIWindowExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIWindowExtensions.swift
+//  EZSwiftExtensions
+//
+//  Created by Goktug Yilmaz on 3/1/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+    /// EZSE: Creates and shows UIWindow. The size will show iPhone4 size until you add launch images with proper sizes. TODO: Add to readme
+    convenience init(viewController: UIViewController, backgroundColor: UIColor) {
+        self.init(frame: UIScreen.mainScreen().bounds)
+        self.rootViewController = viewController
+        self.backgroundColor = backgroundColor
+        self.makeKeyAndVisible()
+    }
+}
+
+
