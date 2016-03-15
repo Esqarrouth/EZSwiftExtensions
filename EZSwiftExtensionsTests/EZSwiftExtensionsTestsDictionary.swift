aca4f976c47cdc11926cc9131186b9bc22312a37
Goktug Yilmaz <goktugyil@gmail.com>
2016-03-15 09:32:54 +0200

    Added test for dictionary multiple union

--- EZSwiftExtensionsTests/EZSwiftExtensionsTestsDictionary.swift
+++ EZSwiftExtensionsTests/EZSwiftExtensionsTestsDictionary.swift
@@ -12,17 +12,23 @@ class EZSwiftExtensionsTestsDictionary: XCTestCase {
   
     var dic: [String:Int]!
     var secondDic: [String:Int]!
+    var thirdDic: [String:Int]!
+
   
     override func setUp() {
         super.setUp()
         dic = ["one" : 1, "two" : 2, "three" : 3]
         secondDic = ["four" : 4, "five" : 5]
+        thirdDic = ["six" : 6, "seven" : 7]
     }
   
     func testUnion() {
         let union = dic.union(secondDic)
         XCTAssertEqual(Array(dic.keys).count + Array(secondDic.keys).count, Array(union.keys).count)
         XCTAssertEqual(Array(dic.values).count + Array(secondDic.values).count, Array(union.values).count)
+        let multiUnion = dic.union(secondDic, thirdDic)
+        XCTAssertEqual(Array(dic.keys).count + Array(secondDic.keys).count + Array(thirdDic.keys).count, Array(multiUnion.keys).count)
+        XCTAssertEqual(Array(dic.values).count + Array(secondDic.values).count + Array(thirdDic.values).count, Array(multiUnion.values).count)
     }
   
 }
