6edb4176c74c64bbc428a7b7698dbe1a70414ebf
Goktug Yilmaz <goktugyil@gmail.com>
2016-03-23 14:02:36 +0200

    Added ErrorType.toString

---
+++ Sources/ErrorTypeExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorTypeExtensions.swift
+//  EZSwiftExtensionsExample
+//
+//  Created by Goktug Yilmaz on 3/23/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+extension ErrorType {
+    /// EZSE: Converts ErrorType value to String.
+    public var toString: String { return String(self) }
+}
