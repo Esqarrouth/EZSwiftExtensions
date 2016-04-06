9e7894ba9332de69a96c9831bf197e53393a50d5
Goktug Yilmaz <goktugyil@gmail.com>
2016-04-06 17:30:04 +0300

    Added x,y,w,h to CGRect

--- Sources/CGRectExtensions.swift
+++ Sources/CGRectExtensions.swift
@@ -13,4 +13,42 @@ extension CGRect {
     public init(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat) {
         self.init(x: x, y: y, width: w, height: h)
     }
+
+    /// EZSE: X value of CGRect's origin
+    public var x: CGFloat {
+        get {
+            return self.origin.x
+        } set(value) {
+            self.origin.x = value
+        }
+    }
+
+    /// EZSE: Y value of CGRect's origin
+    public var y: CGFloat {
+        get {
+            return self.origin.y
+        } set(value) {
+            self.origin.y = value
+        }
+    }
+
+    /// EZSE: Width of CGRect's size
+    public var w: CGFloat {
+        get {
+            return self.size.width
+        } set(value) {
+            self.size.width = value
+        }
+    }
+
+    /// EZSE: Height of CGRect's size
+    public var h: CGFloat {
+        get {
+            return self.size.height
+        } set(value) {
+            self.size.height = value
+        }
+    }
+
 }
+
