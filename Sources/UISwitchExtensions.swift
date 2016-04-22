37d23a34086830f9609e717975f35854fe7b1990
Goktug Yilmaz <goktugyil@gmail.com>
2016-04-22 14:28:13 +0300

    Added UISwitch extensions

---
+++ Sources/UISwitchExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UISwitchExtensions.swift
+//  EZSwiftExtensions
+//
+//  Created by Goktug Yilmaz on 4/22/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+extension UISwitch {
+
+
+
+}
