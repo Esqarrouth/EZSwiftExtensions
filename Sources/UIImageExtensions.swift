bb0f2eaf9016ee811bf9ed5475b27971aff7d956
Amir Abbas Mousavian <a.mosavian@gmail.com>
2016-05-27 15:20:31 +0430

    Update UIImageExtensions.swift
    Colorize image

--- Sources/UIImageExtensions.swift
+++ Sources/UIImageExtensions.swift
@@ -85,6 +85,26 @@ extension UIImage {
         return croppedImage
     }
 
+    /// EZSE: Use current image for pattern of color
+    public func withColor(tintColor: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        
+        let context = UIGraphicsGetCurrentContext()
+        CGContextTranslateCTM(context, 0, self.size.height)
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetBlendMode(context, CGBlendMode.Normal)
+        
+        let rect = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+        CGContextClipToMask(context, rect, self.CGImage)
+        tintColor.setFill()
+        CGContextFillRect(context, rect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+
     ///EZSE: Returns the image associated with the URL
     public convenience init?(urlString: String) {
         guard let url = NSURL(string: urlString) else {
