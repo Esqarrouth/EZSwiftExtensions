aa83e35a185c7e5b4e6fc2a43f099f2049974bcb
Amir Abbas Mousavian <a.mosavian@gmail.com>
2016-06-04 21:09:09 +0430

    Double, String, NSDate, NSURL, NSDictionary, UIApplication and UIImage Extensions and Tests Double: - String inits with precision NSDate: - init from HTTP date strings NSDictionary: - JSON serialize and deserialize NSURL: - file properties methods - get remote server size of URL - checks server supports resuming - comparing URLs String: - determining position of substring - counting words and paragraphs - get matches of regular expression - get height of rendered text (usable in determining UILabel/UITextView height programatically - colorize instances of a substring inside string - hasPrefix and hasSuffix pattern matching UIApplication: - convenience method to run a block when app is in background UIImage: - colorize uiimage (pattern colouring)

--- Sources/StringExtensions.swift
+++ Sources/StringExtensions.swift
@@ -5,9 +5,33 @@
 //  Created by Goktug Yilmaz on 15/07/15.
 //  Copyright (c) 2015 Goktug Yilmaz. All rights reserved.
 //
+#if os(OSX)
+import AppKit
+#else
 import UIKit
+#endif
 
 extension String {
+    /// EZSE: Init string with a base64 encoded string
+    init ? (base64: String) {
+        let pad = String(count: base64.length % 4,repeatedValue: Character("="))
+        let base64Padded = base64 + pad
+        if let decodedData = NSData(base64EncodedString: base64Padded, options: NSDataBase64DecodingOptions(rawValue: 0)), let decodedString = NSString(data: decodedData, encoding: NSUTF8StringEncoding) {
+            self.init(decodedString)
+            return
+        }
+        return nil
+    }
+
+    /// EZSE: base64 encoded of string
+    var base64: String {
+        get {
+            let plainData = (self as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+            let base64String = plainData!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+            return base64String
+        }
+    }
+
     /// EZSE: Cut string from integerIndex to the end
     public subscript(integerIndex: Int) -> Character {
         let index = startIndex.advancedBy(integerIndex)
@@ -54,6 +78,65 @@ extension String {
     /// EZSE: Trims white space and new line characters, returns a new string
     public func trimmed() -> String {
         return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    }
+
+    /// EZSE: Position of begining character of substing
+    public func positionOfSubstring(subString: String, caseInsensitive: Bool = false, fromEnd: Bool = false) -> Int {
+        if subString.isEmpty {
+            return -1
+        }
+        var searchOption = fromEnd ? NSStringCompareOptions.AnchoredSearch : NSStringCompareOptions.BackwardsSearch
+        if caseInsensitive {
+            searchOption.insert(NSStringCompareOptions.CaseInsensitiveSearch)
+        }
+        if let range = self.rangeOfString(subString, options: searchOption) where !range.isEmpty {
+            return self.startIndex.distanceTo(range.startIndex)
+        }
+        return -1;
+    }
+
+    /// EZSE: split string using a spearator string, returns an array of string
+    public func split(separator: String) -> [String] {
+        return self.componentsSeparatedByString(separator).filter {
+            !$0.trimmed().isEmpty
+        }
+    }
+
+    /// EZSE: split string with delimiters, returns an array of string
+    public func split(characters: NSCharacterSet) -> [String] {
+        return self.componentsSeparatedByCharactersInSet(characters).filter {
+            !$0.trimmed().isEmpty
+        }
+    }
+
+    /// EZSE : Returns count of words in string
+    public var countofWords: Int {
+        let regex = try? NSRegularExpression(pattern: "\\w+", options: NSRegularExpressionOptions())
+        return regex?.numberOfMatchesInString(self, options: NSMatchingOptions(), range: NSMakeRange(0, self.length)) ?? 0
+    }
+
+    /// EZSE : Returns count of paragraphs in string
+    public var countofParagraphs: Int {
+        let regex = try? NSRegularExpression(pattern: "\\n", options: NSRegularExpressionOptions())
+        let str = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        return (regex?.numberOfMatchesInString(str, options: NSMatchingOptions(), range: NSMakeRange(0, str.length)) ?? -1) + 1
+    }
+
+    internal func rangeFromNSRange(nsRange : NSRange) -> Range<String.Index>? {
+        let from16 = utf16.startIndex.advancedBy(nsRange.location, limit: utf16.endIndex)
+        let to16 = from16.advancedBy(nsRange.length, limit: utf16.endIndex)
+        if let from = String.Index(from16, within: self),
+            let to = String.Index(to16, within: self) {
+            return from ..< to
+        }
+        return nil
+    }
+
+    /// EZSE: Find matches of regular expression in string
+    public func matchesForRegexInText(regex: String!) -> [String] {
+        let regex = try? NSRegularExpression(pattern: regex, options: [])
+        let results = regex?.matchesInString(self, options: [], range: NSMakeRange(0, self.length)) ?? []
+        return results.map { self.substringWithRange(self.rangeFromNSRange($0.range)!) }
     }
 
     /// EZSE: Checks if String contains Email
@@ -178,11 +261,20 @@ extension String {
         return italicString
     }
     
-    /// EZSE: Returns strikehthrough NSAttributedString
-    public func strikethrough() -> NSAttributedString {
-        let italicString = NSMutableAttributedString(string: self, attributes: [
-        NSStrikethroughStyleAttributeName: NSNumber(integer: NSUnderlineStyle.StyleSingle.rawValue)])
-        return italicString
+    #endif
+
+    #if os(iOS)
+
+    ///EZSE: Returns hight of rendered string
+    func height(width: CGFloat, font: UIFont, lineBreakMode: NSLineBreakMode?) -> CGFloat {
+        var attrib: [String: AnyObject] = [NSFontAttributeName: font]
+        if lineBreakMode != nil {
+            let paragraphStyle = NSMutableParagraphStyle();
+            paragraphStyle.lineBreakMode = lineBreakMode!;
+            attrib.updateValue(paragraphStyle, forKey: NSParagraphStyleAttributeName);
+        }
+        let size = CGSize(width: width, height: CGFloat(DBL_MAX));
+        return ceil((self as NSString).boundingRectWithSize(size, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes:attrib, context: nil).height)
     }
     
     #endif
@@ -191,6 +283,26 @@ extension String {
     public func color(color: UIColor) -> NSAttributedString {
         let colorString = NSMutableAttributedString(string: self, attributes: [NSForegroundColorAttributeName: color])
         return colorString
+    }
+
+    ///EZSE: Returns NSAttributedString
+    public func colorSubString(subString: String, color: UIColor) -> NSMutableAttributedString {
+        var start = 0;
+        var ranges: [NSRange] = []
+        while true {
+            let range = (self as NSString).rangeOfString(subString, options: NSStringCompareOptions.LiteralSearch, range: NSMakeRange(start, (self as NSString).length - start))
+            if range.location == NSNotFound {
+                break;
+            } else {
+                ranges.append(range)
+                start = range.location + range.length
+            }
+        }
+        let attrText = NSMutableAttributedString(string: self);
+        for range in ranges {
+            attrText.addAttribute(NSForegroundColorAttributeName, value: color, range: range)
+        }
+        return attrText;
     }
 
     /// EZSE: Checks if String contains Emoji
@@ -202,5 +314,24 @@ extension String {
             }
         }
         return false
+    }
+}
+
+/// EZSE: Pattern matching of strings via defined functions
+public func ~=<T>(pattern: (T -> Bool), value: T) -> Bool {
+    return pattern(value)
+}
+
+/// EZSE: Can be used in switch-case
+public func hasPrefix(prefix: String) -> (value: String) -> Bool {
+    return { (value: String) -> Bool in
+        value.hasPrefix(prefix)
+    }
+}
+
+/// EZSE: Can be used in switch-case
+public func hasSuffix(suffix: String) -> (value: String) -> Bool {
+    return { (value: String) -> Bool in
+        value.hasSuffix(suffix)
     }
 }
