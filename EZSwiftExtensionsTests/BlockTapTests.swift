5d470345d66fd2ec07e4f30b4018f886186857bb
Goktug Yilmaz <goktugyil@gmail.com>
2016-08-25 20:34:07 +0300

    Added block series tests

---
+++ EZSwiftExtensionsTests/BlockTapTests.swift
@@ -0,0 +1,14 @@
+//
+//  BlockTapTests.swift
+//  EZSwiftExtensions
+//
+//  Created by Valentino Urbano on 28/01/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import XCTest
+
+class BlockTapTests: XCTestCase {
+
+    
+}
