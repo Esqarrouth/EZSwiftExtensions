04803ee7206d312e44d79dab072e4f3b1ebb3f2b
Goktug Yilmaz <goktugyil@gmail.com>
2016-08-25 21:48:27 +0300

    Added CGRect tests

--- EZSwiftExtensionsTests/CGRectTests.swift
+++ EZSwiftExtensionsTests/CGRectTests.swift
@@ -10,7 +10,32 @@ import XCTest
 
 class CGRectTests: XCTestCase {
 
+    func testInit() {
+        let cg = CGRect(x: 55, y: 66, width: 77, height: 88)
+        XCTAssertEqual(cg.origin.x, 55)
+        XCTAssertEqual(cg.origin.y, 66)
+        XCTAssertEqual(cg.width, 77)
+        XCTAssertEqual(cg.height, 88)
+    }
     
+    func testSetXYWH() {
+        var cg = CGRect(x: 55, y: 66, width: 77, height: 88)
+        cg.x = 111
+        cg.y = 112
+        cg.w = 113
+        cg.h = 114
+        XCTAssertEqual(cg.origin.x, 111)
+        XCTAssertEqual(cg.origin.y, 112)
+        XCTAssertEqual(cg.width, 113)
+        XCTAssertEqual(cg.height, 114)
+    }
+    
+    func testGetXYWH() {
+        var cg = CGRect(x: 55, y: 66, width: 77, height: 88)
+        XCTAssertEqual(cg.x, 55)
+        XCTAssertEqual(cg.y, 66)
+        XCTAssertEqual(cg.w, 77)
+        XCTAssertEqual(cg.h, 88)
+    }
 
 }
-
