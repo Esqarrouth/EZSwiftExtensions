a31bdaacd900b811028c465fec808f035a4fbc9d
Goktug Yilmaz <goktugyil@gmail.com>
2016-08-25 20:50:35 +0300

    Added rest of the files

---
+++ EZSwiftExtensionsTests/NSTimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  NSTimerTests.swift
+//  EZSwiftExtensions
+//
+//  Created by Goktug Yilmaz on 8/25/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import XCTest
+
+class NSTimerTests: XCTestCase {
+
+    
+
+}
+
