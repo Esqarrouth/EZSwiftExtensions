c0aca4dcbccbc078f00219410e9633cac70d2afb
Olexii Pyvovarov <piv199@hotmail.com>
2016-08-25 22:07:58 +0300

    ErrorType tests

--- EZSwiftExtensionsTests/ErrorTypeTests.swift
+++ EZSwiftExtensionsTests/ErrorTypeTests.swift
@@ -8,9 +8,44 @@
 
 import XCTest
 
-class ErrorTypeTests: XCTestCase {
-
-    
-
+enum CustomErrorEnum: ErrorType {
+    case CustomError, AnotherError
 }
 
+struct CustomErrorStruct: ErrorType { }
+
+class CustomErrorClass: ErrorType {
+
+    let description: String
+
+    init(description: String) {
+        self.description = description
+    }
+
+    var toString: String {
+        return description
+    }
+}
+
+class ErrorTypeTests: XCTestCase {
+
+    var enumError: CustomErrorEnum!
+    var structError: CustomErrorStruct!
+    var classError: CustomErrorClass!
+
+    override func setUp() {
+        super.setUp()
+        enumError = .CustomError
+        structError = CustomErrorStruct()
+        classError = CustomErrorClass(description: "Custom Error To String")
+    }
+
+    func testToString() {
+        XCTAssertEqual(enumError.toString, "CustomError")
+        enumError = .AnotherError
+        XCTAssertEqual(enumError.toString, "AnotherError")
+        XCTAssertEqual(structError.toString, "CustomErrorStruct")
+        XCTAssertEqual(classError.toString, "Custom Error To String")
+    }
+
+}
