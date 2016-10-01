3325f42c6c6108261f1cc3beff6ead32b73e08c9
Lucas Farah <lucas.farah@me.com>
2016-10-01 01:57:09 -0700

    Small fixes

--- EZSwiftExtensionsTests/CharacterTests.swift
+++ EZSwiftExtensionsTests/CharacterTests.swift
@@ -12,27 +12,25 @@ import XCTest
 
 class CharacterTests: XCTestCase {
     
+    let pchr: Character = "p”
+    let digitChr: Character = “3”
+    
+    let int: Int = 3
+    let str: String = "p"
+
     override func setUp() {
         super.setUp()
     }
     
     func testToString() {
         
-        let chr: Character = "p"
-        let str: String = "p"
+        
         XCTAssertEqual(chr.toString, str)
     }
     
     func testToInt() {
         
-        let chr: Character = "3"
-        let int: Int = 3
         XCTAssertEqual(chr.toInt, int)
-    }
-    
-    func testNotInt() {
-        
-        let chr: Character = "p"
-        XCTAssertEqual(chr.toInt, nil)
+        XCTAssertEqual(pchr.toInt, nil)
     }
 }
