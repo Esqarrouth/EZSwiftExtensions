aa2992fe04a29ed2777ddb9c40faa885481671bc
Lucas Farah <lucas.farah@me.com>
2016-10-01 01:17:34 -0700

    Added Character Tests

--- EZSwiftExtensionsTests/CharacterTests.swift
+++ EZSwiftExtensionsTests/CharacterTests.swift
@@ -8,9 +8,31 @@
 
 import XCTest
 
+@testable import EZSwiftExtensions
+
 class CharacterTests: XCTestCase {
-
     
-
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testToString() {
+        
+        let chr: Character = "p"
+        let str: String = "p"
+        XCTAssertEqual(chr.toString, str)
+    }
+    
+    func testToInt() {
+        
+        let chr: Character = "3"
+        let int: Int = 3
+        XCTAssertEqual(chr.toInt, int)
+    }
+    
+    func testNotInt() {
+        
+        let chr: Character = "p"
+        XCTAssertEqual(chr.toInt, nil)
+    }
 }
-
