e295a919fb8d91163c467f788d33f0b39f2b14c6
Lucas Farah <lucas.farah@me.com>
2016-11-05 12:56:01 -0700

    Added testFrame() and testFontSize()

--- EZSwiftExtensionsTests/UITextFieldTests.swift
+++ EZSwiftExtensionsTests/UITextFieldTests.swift
@@ -7,10 +7,23 @@
 //
 
 import XCTest
+@testable import EZSwiftExtensions
 
 class UITextFieldTests: XCTestCase {
 
-    
+    func testFrame() {
+        
+        let textField = UITextField(x: 30, y: 35, w: 40, h: 45)
+        let expectedFrame =  CGRect(x: 30, y: 35, width: 40, height: 45)
+        
+        XCTAssertEqual(textField.frame, expectedFrame)
+    }
 
+    func testFontSize() {
+        
+        let textField = UITextField(x: 30, y: 35, w: 40, h: 45, fontSize: 38)
+        let expectedFontSize: CGFloat =  38
+        
+        XCTAssertEqual(textField.font?.pointSize, expectedFontSize)
+    }
 }
-
