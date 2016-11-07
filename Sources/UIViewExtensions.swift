0eb44ae9c1002564316bc70564f143bf24712037
albert vila <albert.vp@gmail.com>
2016-11-07 23:30:55 +0100

    Improve methods with default params in definition and inside of the function.

--- Sources/UIViewExtensions.swift
+++ Sources/UIViewExtensions.swift
@@ -599,26 +599,26 @@ extension UIView {
 
 //MARK: Fade Extensions
 
-private let UIViewFadeDuration: TimeInterval = 0.35
+private let UIViewDefaultFadeDuration: TimeInterval = 0.4
 
 extension UIView {
     ///EZSE: Fade in with duration, delay and completion block.
-    public func fadeIn(_ duration:TimeInterval? = UIViewFadeDuration, delay _delay:TimeInterval? = 0.0, completion: ((Bool) -> Void)? = nil) {
-        UIView.animate(withDuration: duration!, delay: _delay!, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
+    public func fadeIn(_ duration:TimeInterval? = UIViewDefaultFadeDuration, delay _delay:TimeInterval? = 0.0, completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: duration ?? UIViewDefaultFadeDuration, delay: _delay ?? 0.0, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
             self.alpha = 1.0
         }, completion:completion)
     }
     
     /// EZSwiftExtensions
-    public func fadeOut(_ duration:TimeInterval? = UIViewFadeDuration, delay _delay:TimeInterval? = 0.0, completion:((Bool) -> Void)? = nil) {
-        UIView.animate(withDuration: duration!, delay: _delay!, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
+    public func fadeOut(_ duration:TimeInterval? = UIViewDefaultFadeDuration, delay _delay:TimeInterval? = 0.0, completion:((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: duration ?? UIViewDefaultFadeDuration, delay: _delay ?? 0.0, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
             self.alpha = 0.0
         }, completion:completion)
     }
     
     /// Fade to specific value	 with duration, delay and completion block.
-    public func fadeTo(_ value:CGFloat, duration _duration:TimeInterval? = UIViewFadeDuration, delay _delay:TimeInterval? = 0.0, completion:((Bool) -> Void)? = nil) {
-        UIView.animate(withDuration: _duration!, delay: _delay!, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
+    public func fadeTo(_ value:CGFloat, duration _duration:TimeInterval? = UIViewDefaultFadeDuration, delay _delay:TimeInterval? = 0.0, completion:((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: _duration ?? UIViewDefaultFadeDuration, delay: _delay ?? UIViewDefaultFadeDuration, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
             self.alpha = value
         }, completion:completion)
     }
