8cc3bb397ec5dcd8e7123dd6cf8c145f8e3dad48
albert vila <albert.vp@gmail.com>
2016-11-10 01:30:31 +0100

    Added test for setTranslation and backToOrigin methods

--- EZSwiftExtensionsTests/UIViewTests.swift
+++ EZSwiftExtensionsTests/UIViewTests.swift
@@ -11,6 +11,51 @@ import XCTest
 
 class UIViewTests: XCTestCase {
 
+    let translation = CGFloat(20)
+
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func view() -> UIView {
+        return UIView(x: 0, y: 0, w: 200, h: 50)
+    }
+    
+    func testSetTranslation() {
+        
+        let view = self.view()
+        let aView = UIView(frame:view.frame)
+        view.addSubview(aView)
+        
+        let oldOrigin:CGPoint = CGPoint(x: aView.frame.minX, y:aView.frame.minY)
+        
+        aView.setTranslation(duration: 1, x: 0, y: translation, completion: nil)
+        
+        let newOrigin = CGPoint(x: aView.frame.minX, y:aView.frame.minY)
+        
+        XCTAssertEqual(oldOrigin.y + translation, newOrigin.y)
+
+    }
+    
+    func testBackToOrigin() {
+        
+        let view = self.view()
+        let aView = UIView(frame:view.frame)
+        view.addSubview(aView)
+        
+        let oldOrigin:CGPoint = CGPoint(x: aView.frame.minX, y:aView.frame.minY)
+        
+        aView.setTranslation(duration: 1, x: translation, y: translation, completion: nil)
+        aView.backToOrigin()
+    
+        XCTAssertEqual(oldOrigin.y, aView.frame.minY)
+    }
+    
     func testRemoveSubviews() {
         
         let view = UIView()
