fa8678d999b2d89a7900ed1eaa7f9cb1165947f7
Arunav Sanyal <arunav.sanyal91@gmail.com>
2016-11-19 23:54:15 -0800

    <Description> : Adding scalar product, subtraction of CGPoints and linear interpolation.
    <Type> : feature/test

--- Sources/CGPointExtensions.swift
+++ Sources/CGPointExtensions.swift
@@ -11,12 +11,22 @@ import Foundation
 
 extension CGPoint {
 
-    /// EZSE: Converts angle degrees to radians.
+    /// EZSE: Adds two CGPoints.
     public static func + (this: CGPoint, that: CGPoint) -> CGPoint {
         return CGPoint(x: this.x + that.x, y: this.y + that.y)
     }
+    
+    /// EZSE: Subtracts two CGPoints.
+    public static func - (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    }
+    
+    /// EZSE: Multiplies a CGPoint with a scalar CGFloat.
+    public static func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+        return CGPoint(x: point.x * scalar, y: point.y * scalar)
+    }
 
-    /// EZSE: Calculates the distance between two CG Points. 
+    /// EZSE: Calculates the distance between two CG Points.
     /// Inspired by : http://stackoverflow.com/questions/1906511/how-to-find-the-distance-between-two-cg-points
     public static func distance(from: CGPoint, to: CGPoint) -> CGFloat {
         return sqrt(pow(to.x - from.x, 2) + pow(to.y - from.y, 2))
@@ -36,5 +46,11 @@ extension CGPoint {
     //// EZSE: Returns the dot product of two vectors represented by points
     public static func dotProduct(this: CGPoint, that: CGPoint) -> CGFloat {
         return this.x * that.x + this.y * that.y
+    }
+    
+    /// EZSE: Performs a linear interpolation between two CGPoint values.
+    /// Inspired by https://github.com/raywenderlich/SKTUtils/blob/master/SKTUtils/CGPoint%2BExtensions.swift
+    public static func linearInterpolation(startPoint: CGPoint, endPoint: CGPoint, interpolationParam: CGFloat) -> CGPoint {
+        return startPoint + (endPoint - startPoint) * interpolationParam
     }
 }
