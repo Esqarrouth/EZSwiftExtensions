deeda0e0e2b1674b76d0d9f33a8392b3f62b3014
albert vila <albert.vp@gmail.com>
2016-12-03 12:38:25 +0100

    new extension for navigationController?.setNavigationBarHidden(hide, animated: true)

--- Sources/UIViewControllerExtensions.swift
+++ Sources/UIViewControllerExtensions.swift
@@ -230,6 +230,11 @@ extension UIViewController {
         _ = navigationController?.popToRootViewController(animated: true)
     }
     
+    /// EZSE: Hide or show navigation bar
+    public func navigationBarHidden(_ hide:Bool) {
+        navigationController?.setNavigationBarHidden(hide, animated: true)
+    }
+    
     /// EZSwiftExtensions
     public func presentVC(_ vc: UIViewController) {
         present(vc, animated: true, completion: nil)
