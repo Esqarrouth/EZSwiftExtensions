6c5df5dcf6cd859737ce3630110b0af8efb5cdbe
Lucas Farah <lucas.farah@me.com>
2017-01-31 01:24:25 -0200

    Added tests for UIImage.base64 NOT WORKING

--- EZSwiftExtensionsTests/UIImageTests.swift
+++ EZSwiftExtensionsTests/UIImageTests.swift
@@ -7,10 +7,22 @@
 //
 
 import XCTest
+@testable import EZSwiftExtensions
 
 class UIImageTests: XCTestCase {
 
-    
+    func testBase64() {
+        let testBundle = Bundle(for: type(of: self))
+        let imageURL = testBundle.url(forResource: "charizard", withExtension: "jpg")
+        let baseURL = testBundle.url(forResource: "base64", withExtension: "txt")
+
+        let dataImage = try? Data(contentsOf: imageURL!)
+
+        let image = UIImage(data: dataImage!)
+        let str = try! String(contentsOfFile: baseURL!.path, encoding: String.Encoding.utf8)
+        
+       XCTAssertEqual(image!.base64, str)
+    }
 
 }
 
