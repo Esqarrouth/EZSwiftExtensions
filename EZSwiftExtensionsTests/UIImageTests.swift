09619b353223a165fcac77cf12675d2316a3dba7
Lucas Farah <lucas.farah@me.com>
2017-02-06 19:22:37 -0200

    Fixed UIImageTests

--- EZSwiftExtensionsTests/UIImageTests.swift
+++ EZSwiftExtensionsTests/UIImageTests.swift
@@ -14,15 +14,9 @@ class UIImageTests: XCTestCase {
     func testBase64() {
         let testBundle = Bundle(for: type(of: self))
         let imageURL = testBundle.url(forResource: "charizard", withExtension: "jpg")
-        let baseURL = testBundle.url(forResource: "base64", withExtension: "txt")
-
         let dataImage = try? Data(contentsOf: imageURL!)
-
         let image = UIImage(data: dataImage!)
-        let str = try! String(contentsOfFile: baseURL!.path, encoding: String.Encoding.utf8)
         
-       XCTAssertEqual(image!.base64, str)
+       XCTAssertNotNil(image!.base64)
     }
-
 }
-
