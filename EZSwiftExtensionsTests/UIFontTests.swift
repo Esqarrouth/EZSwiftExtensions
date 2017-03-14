03269e2180a2ec22be2b503bad9c2f6105e4e6cd
furuyan <k_furuya@waku-2.com>
2017-03-14 16:18:28 +0900

    Fixed test build

--- EZSwiftExtensionsTests/UIFontTests.swift
+++ EZSwiftExtensionsTests/UIFontTests.swift
@@ -13,7 +13,7 @@ class UIFontTests: XCTestCase {
     
     let TEST_SIZE = CGFloat(10)
     
-    let TEST_FONT_TYPE = FontType.Bold
+    let TEST_FONT_TYPE = FontType.bold
     
     override func setUp() {
         super.setUp()
@@ -25,25 +25,25 @@ class UIFontTests: XCTestCase {
     
     func testHelveticaNeueFont() {
         let helveticaNeue = UIFont.HelveticaNeue(type: TEST_FONT_TYPE, size: TEST_SIZE)
-        XCTAssertEqual(helveticaNeue.fontName, FontName.HelveticaNeue.rawValue + "-" + TEST_FONT_TYPE.rawValue)
+        XCTAssertEqual(helveticaNeue.fontName, FontName.helveticaNeue.rawValue + "-" + TEST_FONT_TYPE.rawValue)
         XCTAssertEqual(helveticaNeue.pointSize, TEST_SIZE)
     }
     
     func testAvenirNextFont() {
-        let avenirNext = UIFont.AvenirNext(type: FontType.Bold, size: TEST_SIZE)
-        XCTAssertEqual(avenirNext.fontName, FontName.AvenirNext.rawValue + "-" + TEST_FONT_TYPE.rawValue)
+        let avenirNext = UIFont.AvenirNext(type: FontType.bold, size: TEST_SIZE)
+        XCTAssertEqual(avenirNext.fontName, FontName.avenirNext.rawValue + "-" + TEST_FONT_TYPE.rawValue)
         XCTAssertEqual(avenirNext.pointSize, TEST_SIZE)
     }
     
     func testAvenirNextRegularFont() {
         let avenirNextRegular = UIFont.AvenirNextRegular(size: TEST_SIZE)
-        XCTAssertEqual(avenirNextRegular.fontName, FontName.AvenirNext.rawValue + "-" + FontType.Regular.rawValue)
+        XCTAssertEqual(avenirNextRegular.fontName, FontName.avenirNext.rawValue + "-" + FontType.regular.rawValue)
         XCTAssertEqual(avenirNextRegular.pointSize, TEST_SIZE)
     }
     
     func testAvenirNextDemiBold() {
         let avenirNextDemiBold = UIFont.AvenirNextDemiBold(size: TEST_SIZE)
-        XCTAssertEqual(avenirNextDemiBold.fontName, FontName.AvenirNext.rawValue + "-" + FontType.DemiBold.rawValue)
+        XCTAssertEqual(avenirNextDemiBold.fontName, FontName.avenirNext.rawValue + "-" + FontType.demiBold.rawValue)
         XCTAssertEqual(avenirNextDemiBold.pointSize, TEST_SIZE)
     }
 }
