0a4af45003a18e32a431a88e75a989381070681c
Victor Leong <vljc17@gmail.com>
2017-06-08 03:51:30 +0800

    Text field validations (#429)
    * feat: validateLength of textfield
    * feat: validateEmail
    * feat: test digits only

--- EZSwiftExtensionsTests/UITextFieldTests.swift
+++ EZSwiftExtensionsTests/UITextFieldTests.swift
@@ -15,6 +15,7 @@ class UITextFieldTests: XCTestCase {
     let y: CGFloat = 35
     let width: CGFloat = 40
     let height: CGFloat = 45
+    let testLength = 5
     
     func testFrame() {
         
@@ -30,5 +31,132 @@ class UITextFieldTests: XCTestCase {
         let expectedFontSize: CGFloat =  38
         
         XCTAssertEqual(textField.font?.pointSize, expectedFontSize)
+    }
+    // MARK: Validations
+    func testValidateLength() {
+        let textField = UITextField(x: x, y: y, w: width, h: height, fontSize: 38)
+        var string = ""
+        
+        // empty
+        textField.text = string
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .equalTo))
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .greaterThan))
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .greaterThanOrEqualTo))
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .lessThan))
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .lessThanOrEqualTo))
+        
+        // == testLength
+        for _ in (0..<testLength) {
+            string.append("a")
+        }
+        textField.text = string // "aaaaa" (5 chars)
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .equalTo))
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .greaterThan))
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .greaterThanOrEqualTo))
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .lessThan))
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .lessThanOrEqualTo))
+        
+        // == nil; will be treated as ""
+        textField.text = nil
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .equalTo))
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .greaterThan))
+        XCTAssertFalse(textField.validateLength(ofCount: testLength, option: .greaterThanOrEqualTo))
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .lessThan))
+        XCTAssertTrue(textField.validateLength(ofCount: testLength, option: .lessThanOrEqualTo))
+    }
+    
+    func testValidateEmail() {
+        let textField = UITextField(x: x, y: y, w: width, h: height, fontSize: 38)
+        
+        // empty
+        textField.text = ""
+        XCTAssertFalse(textField.validateEmail())
+        
+        // valid format
+        textField.text = "lol@lol.lol"
+        let acceptableSpecialChars = "!#$%&'*+-/=?^_`{|}~".characters
+        XCTAssertTrue(textField.validateEmail())
+        for specialChar in acceptableSpecialChars {
+            textField.text = "lol\(specialChar)lol@lol.lol"
+            XCTAssertTrue(textField.validateEmail())
+            textField.text = "\(specialChar)lol@lol.lol"
+            XCTAssertTrue(textField.validateEmail())
+            textField.text = "\(specialChar)@lol.lol"
+            XCTAssertTrue(textField.validateEmail())
+            textField.text = "lol\(specialChar)@lol.lol"
+            XCTAssertTrue(textField.validateEmail())
+            for anotherSpecialChar in acceptableSpecialChars {
+                textField.text = "lol\(anotherSpecialChar)\(anotherSpecialChar)lol@lol.lol"
+                XCTAssertTrue(textField.validateEmail())
+                textField.text = "\(anotherSpecialChar)lol\(anotherSpecialChar)@lol.lol"
+                XCTAssertTrue(textField.validateEmail())
+                textField.text = "\(anotherSpecialChar)\(anotherSpecialChar)@lol.lol"
+                XCTAssertTrue(textField.validateEmail())
+                textField.text = "lol\(anotherSpecialChar)lol\(specialChar)@lol.lol"
+                XCTAssertTrue(textField.validateEmail())
+            }
+        }
+        textField.text = "lol.lol@lol.lol"
+        XCTAssertTrue(textField.validateEmail())
+        textField.text = "lol@lol.l"
+        XCTAssertTrue(textField.validateEmail())
+        textField.text = "1lol@lol.lol"
+        XCTAssertTrue(textField.validateEmail())
+        textField.text = "lol1@lol.lol"
+        XCTAssertTrue(textField.validateEmail())
+        textField.text = "lol@lol.lol1"
+        XCTAssertTrue(textField.validateEmail())
+        textField.text = "lol@1.lol"
+        XCTAssertTrue(textField.validateEmail())
+        textField.text = "lol@1.1"
+        XCTAssertTrue(textField.validateEmail())
+        
+        // invalid formats
+        textField.text = "lol.lol.lol"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "lol@lol."
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "lol@lol"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "lol@"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "@"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "@lol"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "@lol."
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "@lol.lol"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = "@lol.lol"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = ".@lol.lol"
+        XCTAssertFalse(textField.validateEmail())
+        textField.text = ".lol@lol.lol"
+        XCTAssertFalse(textField.validateEmail())
+    }
+    
+    func testValidateDigits() {
+        let textField = UITextField(x: x, y: y, w: width, h: height, fontSize: 38)
+        
+        // digits
+        textField.text = "123"
+        XCTAssertTrue(textField.validateDigits())
+        textField.text = "1"
+        XCTAssertTrue(textField.validateDigits())
+        
+        // alphabets
+        textField.text = "l"
+        XCTAssertFalse(textField.validateDigits())
+        textField.text = "lol"
+        XCTAssertFalse(textField.validateDigits())
+        
+        // alphanumerics
+        textField.text = "lol123"
+        XCTAssertFalse(textField.validateDigits())
+        textField.text = "123lol"
+        XCTAssertFalse(textField.validateDigits())
+        textField.text = "1l2o3l"
+        XCTAssertFalse(textField.validateDigits())
     }
 }
--- Sources/UITextFieldExtensions.swift
+++ Sources/UITextFieldExtensions.swift
@@ -11,6 +11,10 @@
 import UIKit
 
 extension UITextField {
+    /// EZSE: Regular exp for email
+    static let emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+    
+
     /// EZSwiftExtensions: Automatically sets these values: backgroundColor = clearColor, textColor = ThemeNicknameColor, clipsToBounds = true,
     /// textAlignment = Left, userInteractionEnabled = true, editable = false, scrollEnabled = false, font = ThemeFontName, fontsize = 17
     public convenience init(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat) {
@@ -48,6 +52,43 @@ extension UITextField {
         self.leftViewMode = UITextFieldViewMode.always
     }
 
-}
+    /// EZSE: Ways to validate by comparison
+    enum textFieldValidationOptions: Int {
+        case equalTo
+        case greaterThan
+        case greaterThanOrEqualTo
+        case lessThan
+        case lessThanOrEqualTo
+    }
+    
+    /// EZSE: Validation length of character counts in UITextField
+    func validateLength(ofCount count: Int, option: UITextField.textFieldValidationOptions) -> Bool {
+        switch option {
+        case .equalTo:
+            return self.text!.characters.count == count
+        case .greaterThan:
+            return self.text!.characters.count > count
+        case .greaterThanOrEqualTo:
+            return self.text!.characters.count >= count
+        case .lessThan:
+            return self.text!.characters.count < count
+        case .lessThanOrEqualTo:
+            return self.text!.characters.count <= count
+        }
+    }
 
+    /// EZSE: Validation of email format based on https://stackoverflow.com/questions/201323/using-a-regular-expression-to-validate-an-email-address and https://stackoverflow.com/questions/2049502/what-characters-are-allowed-in-an-email-address
+    // TODO match String.isEmail method
+    func validateEmail() -> Bool {
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", UITextField.emailRegex)
+        return emailTest.evaluate(with: self.text)
+    }
+    
+    /// EZSE: Validation of digits only
+    func validateDigits() -> Bool {
+        let digitsRegEx = "[0-9]*"
+        let digitsTest = NSPredicate(format:"SELF MATCHES %@", digitsRegEx)
+        return digitsTest.evaluate(with: self.text)
+    }
+}
 #endif
