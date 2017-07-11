cd810e5c78eb128e1c3af9a715a7627042385dbc
Osama Bin Bashir <osama.bashir@mujadidia.com>
2017-07-11 12:58:51 +0500

    Add extension of UIlabel for adding line spacing with the method of name setLineSpacing

--- Sources/UILabelExtensions.swift
+++ Sources/UILabelExtensions.swift
@@ -65,6 +65,19 @@ extension UILabel {
             self.text = _text
         }, completion: nil)
     }
+   
+    // Set lineSpacing for UILabel
+    public func setLineSpacing(lineSpacing: CGFloat) {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 1.0
+        paragraphStyle.lineHeightMultiple = lineHeight
+        paragraphStyle.alignment = self.textAlignment
+        
+        let attrString = NSMutableAttributedString(string: self.text!)
+        attrString.addAttribute(NSFontAttributeName, value: self.font, range: NSMakeRange(0, attrString.length))
+        attrString.addAttribute(NSParagraphStyleAttributeName, value:paragraphStyle, range:NSMakeRange(0, attrString.length))
+        self.attributedText = attrString
+    }
 }
 
 #endif
