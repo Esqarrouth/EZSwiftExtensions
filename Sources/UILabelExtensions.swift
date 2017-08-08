dbe8cddf812d803fa46ddbd600401202c2234442
Arunav Sanyal <arunav.sanyal91@gmail.com>
2017-08-07 20:59:15 -0700

    Minor refactoring on UILabel test and constructor default param.

--- Sources/UILabelExtensions.swift
+++ Sources/UILabelExtensions.swift
@@ -11,13 +11,9 @@
 import UIKit
 
 extension UILabel {
-    /// EZSwiftExtensions: fontSize: 17
-    public convenience init(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat) {
-        self.init(x: x, y: y, w: w, h: h, fontSize: 17)
-    }
-
+    
     /// EZSwiftExtensions
-    public convenience init(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat, fontSize: CGFloat) {
+    public convenience init(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat, fontSize: CGFloat = 17) {
         self.init(frame: CGRect(x: x, y: y, width: w, height: h))
         font = UIFont.HelveticaNeue(type: FontType.None, size: fontSize)
         backgroundColor = UIColor.clear
