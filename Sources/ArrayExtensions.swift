04afcada08fc3a4fa2d1555c939c4fb9aba6c13f
Lucas Farah <lucas.farah@me.com>
2017-08-22 17:28:12 -0300

    Fixed typo

--- Sources/ArrayExtensions.swift
+++ Sources/ArrayExtensions.swift
@@ -219,7 +219,7 @@ extension Array where Element: Hashable {
 
 extension Collection where Indices.Iterator.Element == Index {
     
-    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
     public subscript (safe index: Index) -> Generator.Element? {
         return indices.contains(index) ? self[index] : nil
     }
