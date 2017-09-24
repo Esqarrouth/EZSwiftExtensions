f6d00b0c4df3619f2428d70c31ba8143f67a65bf
Arunav Sanyal <arunav.sanyal91@gmail.com>
2017-09-23 17:00:34 -0700

    Adding a date extractor from user defaults.

--- EZSwiftExtensionsTests/UserDefaultTests.swift
+++ EZSwiftExtensionsTests/UserDefaultTests.swift
@@ -25,5 +25,20 @@ class UserDefaultTests: XCTestCase {
         XCTAssertEqual(testUserDefaults?["foo"] as! String, "bar")
         XCTAssertNil(testUserDefaults?["wrongkey"])
     }
+    
+    func testDate() {
+        let testUserDefaults = UserDefaults(suiteName: "TEST")
+        let key = "dateTestKey"
+        let date = Date()
+        testUserDefaults?.set(date, forKey: key)
+        XCTAssertNotNil(testUserDefaults?.date(forKey: key))
+        XCTAssertEqual(testUserDefaults?.date(forKey: key)!, date)
+        
+        XCTAssertNil(testUserDefaults?.date(forKey: "WRONGKEY"))
+        
+        let keyWithoutDate = "FOO"
+        testUserDefaults?.set("BAR", forKey: keyWithoutDate)
+        XCTAssertNil(testUserDefaults?.date(forKey: keyWithoutDate))
+    }
 }
 
